b30a4d3c40efde2d8dc6d906441aa76160b88acc
Tammy Do <tammytdo11@gmail.com>
2019-02-13 02:53:07 -0800

    commit beginning of group work done in class

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'user strict';
+
+/*
+List of Stores
+    List of hours a store is open paired with total numbers
+
+Store Objects {}
+
+    min_cust
+    max_cust
+    avg_cookies_per_hour
+    store_name
+    store_open: 8
+    store_close: 7
+
+    cookies_sold_each_hour []
+
+    calculate_cookies_per_hour (method)
+    calculate_cookies_all_hours (method)
+    render
+}
+*/
+
+var Alki = {
+  min_cust : 2,
+  max_cust : 17,
+  avg_cookies_per_hour: 4.6,
+  store_name : 'Alki',
+  store_open: 8,
+  store_close: 19
+}
