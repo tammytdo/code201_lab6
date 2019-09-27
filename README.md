324b9de3c43b6930194b2e9130040856d115d43f
tammytdo11 <tammytdo11@gmail.com>
2019-09-27 10:06:34 -0700

    update app.js structure

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
-Cookie Stand
-A cookie sales calculator for the trendy new coffee-time confection called Salmon Cookies. This program predicts the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies are calculated with consideration for the hours of operation and a few factors unique to each location:
+# Salmon Cookies
 
-The minimum number of customers per hour.
-The maximum number of customers per hour.
-The average number of cookies purchased per customer.
+**Author**: Tammy Do
+**Version**: 1.0.5
+
+## Overview
+A cookie sales calculator for the trendy new coffee-time confection called Salmon Cookies. This program predicts the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies are calculated with consideration for the hours of operation and a few factors unique to each location:  
+
+The minimum number of customers per hour.  
+The maximum number of customers per hour.  
+The average number of cookies purchased per customer.  
 
 This adaptable program allows admins to add and remove locations from the daily projections report, and modify the input numbers for each location based on day of the week, special events, and other factors. 
 
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
-Built With
+## Architecture
 HTML5, CSS3, JavaScript
 
-Authors
-Tammy Do
+## Change Log
+<!-- Use this are to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:
 
-Acknowledgments
-Code Fellows
-Nicholas Carignan
-Pat
+01-01-2001 4:59pm - Added functionality to add and delete some things.
+-->
--- js/app.js
+++ js/app.js
@@ -1,58 +1,103 @@
 'use strict';
 
-// Constructor Function
+// Generate a random number
+var _random = function(min, max) {
+  return Math.floor(Math._random() * (max - min) + min);
+};
+
+// Store constructor
 var Store = function(store_name, min_cust, max_cust, store_open, store_close, avg_cookies_per_customer) {
   this.store_name = store_name;
   this.min_cust = min_cust;
   this.max_cust = max_cust;
   this.store_open = store_open;
   this.store_close = store_close;
-  this.cookies_sold_each_hour = [];
   this.avg_cookies_per_customer = avg_cookies_per_customer || 6.3;
+  this.cookies_sold_each_hour = [];
+  this.total_cookies = [];
 };
 
-// Calculates the average number of cookies, per customer, needed each hour
-Store.prototype.cookies_per_hour = function() {
-  var random_customers = Math.floor(Math.random(this.min_cust, this.max_cust));
-  return Math.floor(this.avg_cookies_per_customer * random_customers);
-};
-
-// Calculates the average number of cookies, per customer, needed each hour
+// Avg number of cookies per customer, needed each hour
 Store.prototype.calculate_cookies_per_hour = function() {
-  for (var i = 6; i < 20; i++) { //change to store_open and store_close?
-    var cookies_sold = this.cookies_per_hour();
+  var random_num_customers = _random(this.min_cust, this.max_cust);
+
+  return Math.floor(this.avg_cookies_per_customer * random_num_customers);
+};
+
+// Cookies sold each hour
+Store.prototype.calculate_cookies_sold_each_hour = function() {
+  for (var i = this.store_open; i < this.store_close; i++) {
+    var cookies_sold = this.calculate_cookies_per_hour();
     this.cookies_sold_each_hour.push(cookies_sold);
   }
 };
 
-var render_one_stores_table = function() {
-  this.cookies_sold_each_hour();
-  var target = document.getElementById('cookie-table');
-  var store_row = document.createElement('tr');
-  var name_td = document.createElement('td');
-  name_td.textContent = this.store_name;
-  store_row.appendChild(name_td);
-
+// Cookies sold each hour
+Store.prototype.calculate_total_cookies = function() {
+  var cookieSum = 0;
   for (var i = 0; i < this.cookies_sold_each_hour.length; i++) {
-    var cookie_hour_id = document.createElement('td');
-    cookie_hour_id.textContent = this.cookies_sold_each_hour[i];
-    store_row.appendChild(cookie_hour_id);
+    cookieSum += this.cookies_sold_each_hour[i];
   }
-  target.appendChild(store_row);
+  this.total_cookies.push(cookieSum);
 };
 
+// Render store
+Store.prototype.render = function() {
+  this.calculate_cookies_sold_each_hour();
+  this.calculate_total_cookies();
 
-Store.prototype.render_as_a_table_row = render_one_stores_table;
+  // Build a row of store info
+  var target = document.getElementById('store-container');
+  var tr_el = document.createElement('tr');
+  var td_el = document.createElement('td');
 
-// instantiating
-var firstAndPike = new Store(23, 65, '1st & Pike', 6, 20, 6.3);
-var seaTac = new Store(3, 24, 'SeaTac Airport', 6, 20, 1.2);
-var seattleCenter	= new Store(11, 38, 'Seattle Center', 6, 20, 3.7);
-var capHill	= new Store(20, 38, 'Capitol Hill', 6, 20, 2.3);
-var alki = new Store(2, 16, 'Alki', 6, 20, 4.6);
+  td_el.textContent = this.store_name;
+  tr_el.appendChild(td_el);
 
-var all_locations = [firstAndPike, seaTac, seattleCenter, capHill, alki];
+  td_el = document.createElement('td');
+  td_el.textContent = this.min_cust;
+  tr_el.appendChild(td_el);
 
-for(var i = 0; i < all_locations.length; i++){
-  all_locations[i].render_as_a_table_row();
-}
+  td_el = document.createElement('td');
+  td_el.textContent = this.max_cust;
+  tr_el.appendChild(td_el);
+
+  td_el = document.createElement('td');
+  td_el.textContent = this.store_open;
+  tr_el.appendChild(td_el);
+
+  td_el = document.createElement('td');
+  td_el.textContent = this.store_close;
+  tr_el.appendChild(td_el);
+
+  // Append cookies each hour data
+  td_el = document.createElement('td');
+  td_el.textContent = this.total_cookies + ' cookies';
+  tr_el.appendChild(td_el);
+
+  // Add row to the target element
+  target.appendChild(tr_el);
+};
+
+// Add all store data to one table
+var addDataToArray = function() {
+  var firstAndPike = new Store('1st & Pike', 23, 65, 6, 20, 6.3);
+  var seaTac = new Store('SeaTac Airport', 3, 24, 6, 20, 1.2);
+  var seattleCenter	= new Store('Seattle Center', 11, 38, 6, 20, 3.7);
+  var capHill	= new Store('Capitol Hill', 20, 38, 6, 20, 2.3);
+  var alki = new Store('Alki', 2, 16, 6, 20, 4.6);
+
+  // All store locations
+  var all_locations = [firstAndPike, seaTac, seattleCenter, capHill, alki];
+
+  for(var i = 0; i < all_locations.length; i++){
+    all_locations[i].render();
+  }
+};
+
+var main = function() {
+  addDataToArray();
+};
+
+main();
+
